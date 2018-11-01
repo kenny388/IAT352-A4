7827389f67b5a0f7d20698ab50220311138dc7a1
Unknown <kennycheung388@yahoo.com.hk>
2018-10-31 20:28:27 -0700

    Commented https and fixes not turning off https when successfully logged in

--- callBack_redirect.php
+++ callBack_redirect.php
@@ -4,6 +4,6 @@
   session_start();
   $_SESSION["callback_url"] = '/kycheung/A4/IAT352-A4/addtowatchlist.php';
 
-  //After setting callback, redirect to register page
+  //After setting callback, redirect to register page with https on 
   header("Location: https://" . $_SERVER["HTTP_HOST"] . "/kycheung/A4/IAT352-A4/login.php");
  ?>
--- header.php
+++ header.php
@@ -1,16 +1,19 @@
 <div class="header">
   <div class="nav">
     <ul class="leftNav">
+      <!-- First link in nav bar is unsecure showmodels page -->
       <li><a href="<?php echo 'http://' . $_SERVER["HTTP_HOST"] . '/kycheung/A4/IAT352-A4/showmodels.php'; ?>">All Models</a></li>
-
 
       <?php
       if (isset($_SESSION['loggedIn'])) {
+        //Second link would be watchlist, if logged in, redirecting to unsecure watchlist.php
         echo '<li><a href="http://' . $_SERVER["HTTP_HOST"] . '/kycheung/A4/IAT352-A4/watchlist.php">Watchlist</a></li>';
+        //If logged in, third link would be log out button going to logout.php
         echo '<li><a href="logOut.php">Log Out</a></li>';
         //Say hi to user    o/ Hi!! Can you hear me?       \o hii!!!YES!!!             \o/ \o/ yay! \o/ \o/
         echo "<h3 class='hiUser'>Hi {$_SESSION['firstName']}</h3>";
       } else {
+        //if not logged in, third button would be log in button redirecting to login page with https on
         echo '<li><a href= "https://' . $_SERVER["HTTP_HOST"] . '/kycheung/A4/IAT352-A4/login.php">Login</a></li>';
       }
 
